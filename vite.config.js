5225da155464585aa4bf79ed22cace45a6f764c5
khuranamanan <112554344+khuranamanan@users.noreply.github.com>
2023-08-18 20:19:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://moonshot-product-list-backend.vercel.app/",
+        target: process.env.BACKEND_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
